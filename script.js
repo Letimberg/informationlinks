3f51ad42c5c6b2f8c74ddfb07d331615bc751e0e
letimberg.cavalcante <letimberg.cavalcante@hemoce.ce.gov.br>
2023-10-04 12:48:09 -0300

    Adicinando lista do tipo sanguíneo

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var aborhBtn = document.getElementById("aborhBtn")
+var aborhText = document.getElementById("aborhTable")
+
+aborhBtn.addEventListener("mouseover", function () {
+    aborhText.style.display = "block"
+})
+
+aborhBtn.addEventListener("mouseout", function () {
+    aborhText.style.display = "none"
+})
+
 function toggleMode() {
     const html = document.documentElement
     html.classList.toggle("light")
@@ -8,9 +19,9 @@ function toggleMode() {
     // substituir a imagem
     if (html.classList.contains("light")) {
         // se tiver light mode, adicionar a imagem light
-        img.setAttribute("src", './assets/avatar-light.png')
+        img.setAttribute("src", "./assets/avatar-light.png")
     } else {
         // se tiver sem light mode, manter a imagem normal
-        img.setAttribute("src", './assets/avatar.png')
+        img.setAttribute("src", "./assets/avatar.png")
     }
 }
